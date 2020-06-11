05bfda6500dbca097144e1deafebe1a698ce3b7b
Jerome Toulouse <toulouse.jerome@gmail.com>
2020-06-11 17:08:17 +0200

    Routing, Controller, Views for Doses

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+Cocktail.create(name: "Lemonade")
+Cocktail.create(name: "Gin To")
+Cocktail.create(name: "Cuba Libre")
